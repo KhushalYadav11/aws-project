deddc284ba7520987fee61b5f67cbfb431eac395
Khushal Yadav <130129742+KhushalYadav11@users.noreply.github.com>
2024-08-26 23:40:07 +0530

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -6,7 +6,6 @@ from config import *
 
 app = Flask(__name__)
 
-# Database connection (replace with your actual database details)
 db_conn = pymysql.connect(
     host=customhost,
     user=customuser,
@@ -15,16 +14,13 @@ db_conn = pymysql.connect(
     port=3306
 )
 
-# Define the S3 bucket name and region
 custombucket = custombucket
 region = customregion
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    # Debugging statements
     print("Current working directory:", os.getcwd())
     print("Templates directory contents:", os.listdir('templates'))
-    
     return render_template('AddEmp.html')
 
 @app.route("/about", methods=['GET'])
@@ -50,28 +46,23 @@ def AddEmp():
         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
 
-        emp_name = "" + first_name + " " + last_name
+        emp_name = first_name + " " + last_name
 
-        # Upload image file in S3
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
-            
-            # Upload the image to S3
             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
 
-            # Get the location of the S3 bucket
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
+            s3_location = bucket_location['LocationConstraint']
 
             if s3_location is None:
                 s3_location = ''
             else:
                 s3_location = '-' + s3_location
 
-            # Construct the object URL
             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
                 s3_location,
                 custombucket,
